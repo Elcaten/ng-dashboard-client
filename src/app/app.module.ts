82894c1cb266be02c19152322651d090096f7400
Andrew Lineyschikov <andrew.lineyshikov@gmail.com>
2018-07-10 00:26:59 +0300

    Installed DevExtreme framework

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { DxButtonModule } from 'devextreme-angular';
+
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +10,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DxButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
